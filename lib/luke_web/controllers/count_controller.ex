5ec4a3a082afa40ce22201210e993d8e9d6dd18a
mfrasca <mario@anche.no>
2019-06-19 07:57:54 -0500

    added random waiting time for function that does nothing

--- lib/luke_web/controllers/count_controller.ex
+++ lib/luke_web/controllers/count_controller.ex
@@ -2,7 +2,10 @@ defmodule LukeWeb.CountController do
   use LukeWeb, :controller
 
   def index(conn, _params) do
-    users = %{"__total__" => 0}
-    json conn, users
+    sleeping = :crypto.rand_uniform(500, 5000)
+    :timer.sleep(sleeping)
+    result = %{"__total__" => sleeping}
+    json conn, result
   end
 end
+
